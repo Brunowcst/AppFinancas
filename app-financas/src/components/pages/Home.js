928b1b99490ab2c5352eaf2c3cd51ab8a9c282e9
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-12 19:18:04 -0300

    Página Home

--- app-financas/src/components/pages/Home.js
+++ app-financas/src/components/pages/Home.js
@@ -1,8 +1,16 @@
+//import styles from "./Home.modules.css";
+import "./Home.modules.css";
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className="section_home">
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Gerencie agora seus projetos de maneira ágil e fácil!</p>
+            <LinkButton to="newproject" text="Criar Projeto"/>
+            <img className="img_savings" src={savings} alt="imagem savings"/>
+        </section>
     )
 }
 
