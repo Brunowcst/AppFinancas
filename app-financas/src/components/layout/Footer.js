c766affed83a86b809a20a7e9725966ccca6bf98
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-11 21:56:16 -0300

    Links para redes socias no footer

--- app-financas/src/components/layout/Footer.js
+++ app-financas/src/components/layout/Footer.js
@@ -7,8 +7,13 @@ function Footer() {
         <footer>
             <ul className='lista_icons'>
                 <li className='item_icon'><FaFacebook/></li>
-                <li className='item_icon'><FaInstagram/></li>
-                <li className='item_icon'><FaGithub/></li>
+
+                <a href="https://www.instagram.com/brwnocp/" target="_blank">
+                    <li className='item_icon'><FaInstagram/></li>
+                </a>
+                <a href="https://github.com/Brunowcst" target="_blank">
+                    <li className='item_icon'><FaGithub/></li>
+                </a>
             </ul>
             <p className='copyright'><span className='span_costs'>Costs</span> &copy; 2023</p>
         </footer>
