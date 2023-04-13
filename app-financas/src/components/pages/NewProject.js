8a69a511b89f54cb06ad68221da6ecb354b30dde
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-13 20:24:08 -0300

    Página de cadastro

--- app-financas/src/components/pages/NewProject.js
+++ app-financas/src/components/pages/NewProject.js
@@ -1,8 +1,13 @@
+import ProjectForm from "../project/ProjectForm";
+import "./NewProject.modules.css"
+
 function NewProject() {
     return (
-        <div>
-            New
-        </div>
+        <div className="cad_projeto">
+            <h1>Crie seu projeto</h1>
+            <p>Cadastre um projeto para adicionar serviços a ele.</p>
+            <ProjectForm btnText="Cadastrar projeto"/> 
+        </div> 
     );
 }
 
