85c1c3b1ed2fc86b1662b91feee383274d577551
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-13 20:19:21 -0300

    Componente genérico: Input

---
+++ app-financas/src/components/form/Input.js
@@ -0,0 +1,16 @@
+import './Input.modules.css';
+
+function Input({type, text, placeholder, name, value, handleOnChange}) {
+    return (
+        <div className="input_container">
+            <label htmlFor={name}>{text}</label>
+            <input type={type} 
+            name={name} 
+            placeholder={placeholder}
+            onChange={handleOnChange}
+            value={value}></input>
+        </div>
+    );
+}
+
+export default Input;
